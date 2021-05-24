54eb1a7f1059c144c1635ba79fb90355d2b2a106
benardoneka <benardoneka@gmail.com>
2021-05-24 13:09:58 +0300

    I have just added a line of code with my name

--- src/HelloFromRufus.java
+++ src/HelloFromRufus.java
@@ -2,5 +2,6 @@ public class HelloFromRufus {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Rufus");
+        System.out.println("Hello Rufus, my name is Benard from your Group");
     }
 }
